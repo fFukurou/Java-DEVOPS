e4348d8bb685daffc01c23dc46b5aa52538bc7bc
fFukurou <supevi1@gmail.com>
2025-05-06 20:19:45 -0300

    initial files

---
+++ src/main/java/fiap/com/sensorium/SensoriumApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.sensorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensoriumApplication.class, args);
+	}
+
+}
